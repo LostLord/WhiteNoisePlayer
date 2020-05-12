55ae2a18e6346f40cf2fe7538a3e72629de00cf3
Howyiran <1098786596@qq.com>
2020-05-12 19:39:30 +0800

    use test

--- app/src/test/java/com/softwareproject/whitenoiseplayer/ExampleUnitTest.kt
+++ app/src/test/java/com/softwareproject/whitenoiseplayer/ExampleUnitTest.kt
@@ -6,12 +6,19 @@ import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- *
+ * test&test&test&test&test&test&test OHHHHHHHHHHHHHH
+ * 不知道写什么注释 那就随便吹吹彩虹屁叭
+ *  .(＞д＜). o(≧ヘ≦)o (>口＜-) (＞﹏＜)　(>_<)　(>.<)　(>o<)　(;>△<)　(＞д＜)　(>皿<)（≧▽≦） ○(￣﹏￣)○ 　(。>︿<)　　（ =￣ω￣=） o((⊙﹏⊙))o. (*￣(エ)￣)   (*｀∧´)(´ω｀*)    (⊙ˍ⊙) ●︿● ◑△◐  ＞▂＜ (*^︹^*)
+ * 噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗噗
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+        /**
+         * 4=2+2！！！！！！！！Amazing！
+         */
+        /
     }
 }
