d502c9fe51b5b0e2989a4eba1cd20ae0e6ae93b9
cht <1435901016@qq.com>
2020-05-19 00:16:09 +0800

    fin

--- app/src/test/java/com/softwareproject/whitenoiseplayer/ExampleUnitTest.kt
+++ app/src/test/java/com/softwareproject/whitenoiseplayer/ExampleUnitTest.kt
@@ -14,8 +14,14 @@ import org.junit.Assert.*
  */
  
  /*
- *ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
- *ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+ *
+ *1.（经济）全球化对发展中国家或发达国家（资本主义国家或社会主义国家）的利弊，最好有数据支撑
+2.去全球化的原因（可否避免）、可能会造成的影响，去全球化是否有可能
+3.发达国家和发展中国家经济体系和全球化的关系
+4.石油美元体系，当前处境中国是否有可能创造出一个新的模式
+5.疫情影响下的经济全球化状况以及二者关系，全球化对公共卫生或他产业链的影响
+6.中国如何应对疫情带来的全球化考验？（政策）
+
  */
 class ExampleUnitTest {
     @Test
